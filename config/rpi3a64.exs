4421251aea7de873d3e7dd739e4c2d149d2860f7
Edgar Gomes <talktoedgar@gmail.com>
2023-01-20 01:39:12 -0500

    set target to rpi3a 64bit

---
+++ config/rpi3a64.exs
@@ -0,0 +1,13 @@
+import Config
+
+# Configure the network using vintage_net
+# See https://github.com/nerves-networking/vintage_net for more information
+config :vintage_net,
+  config: [
+    {"usb0", %{type: VintageNetDirect}},
+    {"wlan0", %{type: VintageNetWiFi}}
+  ]
+
+config :delux, indicators: %{default: %{green: "led0"}}
+
+config :nerves, :firmware, fwup_conf: "config/rpi3a64/fwup.conf"

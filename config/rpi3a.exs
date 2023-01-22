f7470e95e00959e526e1e3e0916db248ddc30c31
Edgar Gomes <talktoedgar@gmail.com>
2023-01-22 15:02:26 -0500

    Add WIFI config

--- config/rpi3a.exs
+++ config/rpi3a.exs
@@ -5,7 +5,18 @@ import Config
 config :vintage_net,
   config: [
     {"usb0", %{type: VintageNetDirect}},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0",
+     %{
+       type: VintageNet.Technology.WiFi,
+       wifi: %{
+         key_mgmt: System.get_env("WIFI_KEY_MANAGEMENT", "wpa_psk") |> String.to_atom(),
+         ssid: System.fetch_env!("WIFI_SSID"),
+         psk: System.fetch_env!("WIFI_PSK")
+       },
+       ipv4: %{
+         method: :dhcp
+       }
+     }}
   ]
 
 config :delux, indicators: %{default: %{green: "led0"}}

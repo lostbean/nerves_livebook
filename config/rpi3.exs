b2dbd9926bb4acd66977d14fdafc83e4104efefc
lostbean <talktoedgar@gmail.com>
2024-02-14 22:59:59 -0300

    compile with wifi info

--- config/rpi3.exs
+++ config/rpi3.exs
@@ -5,7 +5,20 @@ import Config
 config :vintage_net,
   config: [
     {"eth0", %{type: VintageNetEthernet, ipv4: %{method: :dhcp}}},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0",
+     %{
+       type: VintageNetWiFi,
+       vintage_net_wifi: %{
+         networks: [
+           %{
+             key_mgmt: System.get_env("WIFI_KEY_MANAGEMENT", "wpa_psk") |> String.to_atom(),
+             ssid: System.fetch_env!("WIFI_SSID"),
+             psk: System.fetch_env!("WIFI_PSK")
+           }
+         ]
+       },
+       ipv4: %{method: :dhcp}
+     }}
   ]
 
 config :delux, indicators: %{default: %{green: "ACT"}}
--- config/rpi3a.exs
+++ config/rpi3a.exs
@@ -5,7 +5,20 @@ import Config
 config :vintage_net,
   config: [
     {"usb0", %{type: VintageNetDirect}},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0",
+     %{
+       type: VintageNetWiFi,
+       vintage_net_wifi: %{
+         networks: [
+           %{
+             key_mgmt: System.get_env("WIFI_KEY_MANAGEMENT", "wpa_psk") |> String.to_atom(),
+             ssid: System.fetch_env!("WIFI_SSID"),
+             psk: System.fetch_env!("WIFI_PSK")
+           }
+         ]
+       },
+       ipv4: %{method: :dhcp}
+     }}
   ]
 
 config :delux, indicators: %{default: %{green: "ACT"}}

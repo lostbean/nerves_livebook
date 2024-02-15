649fbb4172eccccf605a811e6f10c30dd708eaa3
lostbean <talktoedgar@gmail.com>
2024-02-14 22:57:42 -0300

    use same erlang/elixir versions

--- flake.nix
+++ flake.nix
@@ -13,18 +13,21 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         unstable_pkgs = unstable.legacyPackages.${system};
+        otp = pkgs.beam.packages.erlangR26;
       in {
         formatter = pkgs.nixpkgs-fmt;
 
         devShell = pkgs.mkShell {
+          # keep your shell history in iex
+          ERL_AFLAGS = "-kernel shell_history enabled";
+
           nativeBuildInputs = with pkgs;
             let
               frameworks = darwin.apple_sdk.frameworks;
               inherit (lib) optional optionals;
             in [
               # Dev environment
-              elixir_1_16
-              erlang_26
+              otp.elixir_1_16
               elixir_ls
               rebar3
               fwup
@@ -48,6 +51,7 @@
               pkg-config
               inotify-tools
               libmnl
+              usbutils
             ] ++ optionals stdenv.isDarwin [
               # add macOS headers to build mac_listener and ELXA
               frameworks.CoreServices
@@ -61,7 +65,17 @@
 
           shellHook = ''
             SUDO_ASKPASS=${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass
-            mix archive.install hex nerves_bootstrap
+
+            # local directory
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+
+            export MIX_PATH="${otp.hex}/lib/erlang/lib/hex/ebin"
+            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+
+            mix archive.install hex nerves_bootstrap --force
+
             printf '\u001b[32m
             The Bauhaus!
             \e[0m
